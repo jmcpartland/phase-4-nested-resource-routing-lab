7461d72bca2d2ca6f3dbb4f1ecd05235f6cb35cc
Joseph McPartland <joemcpartland@gmail.com>
2022-08-09 14:37:28 -0600

    commit tree f7627ac86dfa8a12eb566b51c24394d843adac4f

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index] do
+    resources :items, only: [:show, :index, :create]  
+  end
+  resources :items, only: [:show, :index, :create]
 end
